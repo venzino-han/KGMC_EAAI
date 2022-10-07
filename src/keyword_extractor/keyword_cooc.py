28efb521ed824c76d18ac79c240191cc42d5a5a5
kirc <virgu1991@gmail.com>
2022-10-07 21:21:29 +0900

    add cooc edge limit

--- src/keyword_extractor/keyword_cooc.py
+++ src/keyword_extractor/keyword_cooc.py
@@ -2,13 +2,21 @@ from collections import defaultdict
 import pandas as pd
 import pickle as pkl
 from itertools import combinations, product
+import gc
+import time
+from random import sample
+
+EDGE_LIMIT = 1024
 
 def get_id_keyword_dict(df)->defaultdict:
     keyword_id_dict = defaultdict(set)
-    for row in df.iterrows():
-        id, kws = row[0], row[1].split()
+    for _, row in df.iterrows():
+        id = row.doc_id
+        try: kws = row.keywords.split()
+        except: kws = []
         for kw in kws:
             keyword_id_dict[kw].add(id)
+    print('keyword id dict')
     return keyword_id_dict
     
 def get_cooc_pair_df(keyword_id_dict)->pd.DataFrame:
@@ -16,24 +24,28 @@ def get_cooc_pair_df(keyword_id_dict)->pd.DataFrame:
     return cooc edge as df format
     uid, vid, cooc_count
     """
-    pair_dict = defaultdict(int)
+    pair_set = set()
+    us, vs = [], []
     for kw, ids in keyword_id_dict.items():
-        for u, v in combinations(ids, 2):
+        # if len(ids) > 1024:
+        #     ids = sample(ids, 1024)
+
+        pairs = list(combinations(ids, 2))
+        if len(pairs) > EDGE_LIMIT:
+            pairs = sample(pairs, EDGE_LIMIT)
+        for u, v in pairs:
             if u > v:
                 u,v=v,u
-            pair_dict[(u,v)] += 1
+            if (u,v) not in pair_set:
+                pair_set.add((u,v))        
+                us.append(u)
+                vs.append(v)
 
-    us, vs, cs = [], [], []
-    for k, c in pair_dict.items():
-        u, v = k 
-        us.append(u)
-        vs.append(v)
-        cs.append(c)
-
+    print('homo pair')
     return pd.DataFrame({
         'u':us,
         'v':vs,
-        'c':cs,
+        # 'c':cs,
     })
 
 
@@ -42,72 +54,66 @@ def get_uv_cooc_pair_df(u_dict, v_dict)->pd.DataFrame:
     return cooc edge between `user-item` as df format
     uid, vid, cooc_count
     """
-    pair_dict = defaultdict(int)
+    pair_set = set()
     for kw, u_ids in u_dict.items():
         if kw not in v_dict:
             continue
         v_ids = v_dict.get(kw,set())
-        for u, v in product(u_ids, v_ids):
-            pair_dict[(u,v)] += 1
+        pairs = list(product(u_ids, v_ids))
+        if len(pairs) > EDGE_LIMIT:
+            pairs = sample(pairs, EDGE_LIMIT)
+        for u, v in pairs:
+            if (u,v) not in pair_set:
+                pair_set.add((u,v))
 
     us, vs, cs = [], [], []
-    for k, c in pair_dict.items():
-        u, v = k 
+    for u, v in pair_set:
         us.append(u)
         vs.append(v)
-        cs.append(c)
+        # cs.append(c)
 
+    print('user-item pair')
     return pd.DataFrame({
         'u':us,
-        'v':vs,
-        'c':cs,
+        'i':vs,
+        # 'c':cs,
     })
 
 
 if __name__=='__main__':
-    for keyword_extraction_method in ['text_rank','topic_rank']:
+    for keyword_extraction_method in [
+                'text_rank',
+                'keybert', 
+                'tfidf',        
+                # 'topic_rank',
+        ]:
         for data_name in [
                         'yelp',
-                        'grocery',
-                        'epinions',
-                        'games', 
-                        'music', 
-                        'office', 
-                        'sports',
+                        # 'epinions',
+                        # 'grocery',
+                        # 'games', 
+                        # 'music', 
+                        # 'office', 
+                        # 'sports',
                         ]:
             
             item_df = pd.read_csv(f'data/{data_name}/item_{keyword_extraction_method}_keywords.csv')
             user_df = pd.read_csv(f'data/{data_name}/user_{keyword_extraction_method}_keywords.csv')
 
-            cooc_dict = get_id_keyword_dict(item_df)
-            cooc_df = get_cooc_pair_df(cooc_dict)
+            item_dict = get_id_keyword_dict(item_df)
+            cooc_df = get_cooc_pair_df(item_dict)
             cooc_df.to_csv(f'data/{data_name}/item_{keyword_extraction_method}_cooc.csv')
+            del cooc_df
+            gc.collect()
+            print('item', time.time())
 
-            cooc_dict = get_id_keyword_dict(user_df)
-            cooc_df = get_cooc_pair_df(cooc_dict)
+            user_dict = get_id_keyword_dict(user_df)
+            cooc_df = get_cooc_pair_df(user_dict)
             cooc_df.to_csv(f'data/{data_name}/user_{keyword_extraction_method}_cooc.csv')
+            del cooc_df
+            gc.collect()
+            print('user', time.time())
 
-            cooc_dict = get_id_keyword_dict(user_df)
-            cooc_df = get_uv_cooc_pair_df(cooc_dict)
+            cooc_df = get_uv_cooc_pair_df(user_dict, item_dict)
+            print('user-item', time.time())
             cooc_df.to_csv(f'data/{data_name}/user_item_{keyword_extraction_method}_cooc.csv')
-
-
-            # keyword_item_dict = defaultdict(set)
-            # keyword_user_dict = defaultdict(set)
-
-            # for row in item_df.iterrows():
-            #     iid, kws = row.item_id, row.keywords.split()
-            #     for kw in kws:
-            #         keyword_item_dict[kw].add(iid)
-
-            # for row in user_df.iterrows():
-            #     uid, kws = row.user_id, row.keywords.split()
-            #     for kw in kws:
-            #         keyword_user_dict[kw].add(uid)
-
-            
-
-            # with open(f'data/{data_name}/item_{keyword_extraction_method}_kw_dict.pkl', 'wb') as f:
-            #     pkl.dump(keyword_item_dict, f)
-            # with open(f'data/{data_name}/user_{keyword_extraction_method}_kw_dict.pkl', 'wb') as f:
-            #     pkl.dump(keyword_user_dict, f)            
